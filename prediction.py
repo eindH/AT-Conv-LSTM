3444f2e5b7360df1e283df8be0e75cc339d1580d
eindH <32923596+eindH@users.noreply.github.com>
2024-02-28 16:26:00 +0100

    Model to predict rain attenuation
    Model predicts rain attenuation. Doesn't use weekly or daily data. Instead uses data from random points in the past.

--- prediction.py
+++ prediction.py
@@ -26,13 +26,12 @@ from keras.callbacks import Callback
 #config = tf.ConfigProto()
 #config.gpu_options.allow_growth = True
 print('loading data...')
-data1 = load_csv(r'data-freeway/10105110', 8, "freeway")
-data2 = load_csv(r'data-freeway/10105310', 8, "freeway")
-data3 = load_csv(r'data-freeway/10105510', 8, "freeway")
-data4 = load_csv(r'data-freeway/10108210', 8, "freeway")
-data5 = load_csv(r'data-freeway/10106510', 8, "freeway")
-data6 = load_csv(r'data-freeway/1095110', 8, "freeway")
-data7 = load_csv(r'data-freeway/1095510', 8, "freeway")
+data1 = load_csv(r'rain_attenuation', 6, "rain")
+data2 = load_csv(r'rain_attenuation', 2, "rain")
+data3 = load_csv(r'rain_attenuation', 3, "rain")
+data4 = load_csv(r'rain_attenuation', 4, "rain")
+data5 = load_csv(r'rain_attenuation', 5, "rain")
+data6 = load_csv(r'rain_attenuation', 1, "rain")
 
 # data1 = load_csv(r'data-urban/401190', 5, "urban")
 # data2 = load_csv(r'data-urban/401144', 7, "urban")
@@ -52,7 +51,7 @@ pre_sens_num = 1
 
 #train,test
 train_data, train_w, train_d, label, test_data, test_w, test_d, test_l, test_med, test_min\
-	= generate_data(data1, data2, data3, data4, data5, data6, data7, seq_len, pre_len, pre_sens_num)
+	= generate_data(data1, data2, data3, data4, data5, data6, seq_len, pre_len, pre_sens_num)
 
 train_data = np.reshape(train_data,(train_data.shape[0], train_data.shape[1], train_data.shape[2], 1))
 train_w = np.reshape(train_w,(train_w.shape[0], train_w.shape[1], 1))
